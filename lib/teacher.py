7b59655ab1c955f0eb97a33adc5d68d3cec05951
nuru999 <70575815+nuru999@users.noreply.github.com>
2023-11-28 19:46:20 +0300

    completed

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,21 @@ from user import User
 import random
 
 class Teacher(User):
-
+    def __init__(self,first_name, last_name,knowledge=None):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "str is a data type in Python",
+            "Programming is a valuable skill",
+            "Python function definition",
+            "Object-oriented programming concepts",
+            "Python libraries and frameworks",
+            "Web development with JavaScript",
+            "Database design and management",
+            "Software testing and debugging",
+        ] 
+   
     def teach(self):
-        pass
+        if self.knowledge:
+            return random.choice(self.knowledge)
+        else:
+            return None
